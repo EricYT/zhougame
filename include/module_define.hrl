51657e51602fffb392f96b90e4376902fa3cafdd
EricYT <eric.yutao@gmail.com>
2014-04-24 23:01:53 +0800

    modified:   include/module_define.hrl 	modified:   src/mysql_code_gen/mysql_test.config

--- include/module_define.hrl
+++ include/module_define.hrl
@@ -3,4 +3,9 @@
 -record(module_define, {module_name, columns, primary_key, index, engine}).
 
 %%
--record(columns_define, {col_name, type, auto_increment, is_null, default, description}).
+-record(columns_define, {col_name, type, length, is_null, default, description}).
+
+%% proto
+-record(proto_define, {proto_name, columns, key, type}).
+
+-record(attr_define, {col_name, type, description}).
