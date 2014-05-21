169cc7483817aca8b929973482ec9e91b354329f
EricYT <eric.yutao@gmail.com>
2014-05-21 12:34:41 +0800

    add login_pb.erl

---
+++ src/proto/login_pb.erl
@@ -0,0 +1,24 @@
+%% Author: cb1224
+%% Created: 2014-5-21
+%% Description: TODO: Add description to login_pb
+-module(login_pb).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([]).
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
