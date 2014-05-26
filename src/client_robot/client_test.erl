59640f2629d1724f45e3b148c798d947a5296890
EricYT <eric.yutao@gmail.com>
2014-05-26 22:30:18 +0800

    debug

--- src/client_robot/client_test.erl
+++ src/client_robot/client_test.erl
@@ -13,25 +13,28 @@
 
 %% --------------------------------------------------------------------
 %% External exports
--export([start_link/1, send_data/1]).
+-export([start_link/1, send_data/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {socket}).
+-record(state, {client_id, socket}).
 
 %% ====================================================================
 %% External functions
 %% ====================================================================
-start_link([]) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+start_link([ClientId]) when is_list(ClientId) ->
+    start_link([list_to_atom(ClientId)]);
+start_link([ClientId]) when is_atom(ClientId) ->
+    gen_server:start_link({local, ClientId}, ?MODULE, [ClientId], []).
 
 
--spec send_data(Msg) -> 'ok' | {error, Error} when
-                                                Msg :: term(),
-                                                Error :: atom().
-send_data(Msg) ->
-    erlang:send(?MODULE, {send_data, Msg}).
+-spec send_data(ClientId, Msg) -> 'ok' | {error, Error} when
+                                                          ClientId :: integer(),
+                                                          Msg :: term(),
+                                                          Error :: atom().
+send_data(ClientId, Msg) when is_atom(ClientId) ->
+    erlang:send(ClientId, {send_data, Msg}).
 
 %% ====================================================================
 %% Server functions
@@ -45,10 +48,10 @@ send_data(Msg) ->
 %%          ignore               |
 %%          {stop, Reason}
 %% --------------------------------------------------------------------
-init([]) ->
+init([ClientId]) ->
     {ok, Socket} = gen_tcp:connect("127.0.0.1", 8089, [{packet, 2}, binary]),
     io:format(">>>>>>>>>>>> ~p~n", [{?MODULE, ?LINE, Socket}]),
-    {ok, #state{socket = Socket}}.
+    {ok, #state{client_id = ClientId, socket = Socket}}.
 
 %% --------------------------------------------------------------------
 %% Function: handle_call/3
