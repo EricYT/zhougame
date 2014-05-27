076813b8fbc8b4572d5ad66247eecaafafc15116
EricYT <eric.yutao@gmail.com>
2014-05-27 14:52:03 +0800

    modified:   .project 	modified:   .settings/org.erlide.core.prefs 	modified:   src/client_robot/client_test.erl 	new file:   src/test/test.erl

---
+++ src/test/test.erl
@@ -0,0 +1,17 @@
+%% @author cb1224
+%% @doc @todo Add description to test.
+
+
+-module(test).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([]).
+-compile(export_all).
+
+test_map() ->
+	todo.
+
+test_fun() ->
+	todo.
