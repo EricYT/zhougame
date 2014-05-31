71f6d34324ae06b0ea9a7be5bc2f9f9b008bfe1b
EricYT <eric.yutao@gmail.com>
2014-05-31 16:47:11 +0800

    change proto way

--- src/proto/parse_proto_data.erl
+++ src/proto/parse_proto_data.erl
@@ -13,28 +13,42 @@
 
 -define(PROTO_FILE, "../src/proto/login_pb.proto").
 
+-define(MESSAGE_HEAD_ETS, 'ets_message_head').
+-define(MESSAGE_DEFINE_ETS, 'ets_message_define').
+-define(MESSAGE_TYPE_ETS, 'ets_message_type').
+
 parse_proto_data() ->
     case file:consult(?PROTO_FILE) of
         {ok, Messages} ->
-            convert_message_to_record(Messages, [], [], []),
+            {Msgs, MsgDefines, MsgTypes} =
+                message_validate(Messages, [], [], []),
+            init_message_ets(Msgs, MsgDefines, MsgTypes),
+            io:format("Message : ~p~n", [{Msgs, MsgDefines, MsgTypes}]),
             ok;
         Error ->
             io:format(">>>>>> Open file faild:~p~n", [{?MODULE, ?LINE, Error}])
     end.
 
 
-convert_message_to_record([], Msgs, MsgDefines, MsgTypes) ->
+message_validate([], Msgs, MsgDefines, MsgTypes) ->
     {Msgs, MsgDefines, MsgTypes};
-convert_message_to_record([#message_heads{}=Record|Tail],
+message_validate([#message_heads{}=Record|Tail],
                           Msgs, MsgDefines, MsgTypes) ->
-    io:format("message_heads ~p~n", [Record]),
-    convert_message_to_record(Tail, Msgs, MsgDefines, MsgTypes);
-convert_message_to_record([#msg_normal{}=Record|Tail],
+    message_validate(Tail, [Record|Msgs], MsgDefines, MsgTypes);
+message_validate([#msg_normal{}=Record|Tail],
                           Msgs, MsgDefines, MsgTypes) ->
-    io:format("msg_normal ~p~n", [Record]),
-    convert_message_to_record(Tail, Msgs, MsgDefines, MsgTypes);
-convert_message_to_record([#type_private{}=Record|Tail],
+    message_validate(Tail, Msgs, [Record|MsgDefines], MsgTypes);
+message_validate([#type_private{}=Record|Tail],
                           Msgs, MsgDefines, MsgTypes) ->
-    io:format("type_private ~p~n", [Record]),
-    convert_message_to_record(Tail, Msgs, MsgDefines, MsgTypes).
+    message_validate(Tail, Msgs, MsgDefines, [Record|MsgTypes]).
 
+
+init_message_ets(MsgHeads, MsgDefines, MsgType) ->
+    try
+        ets:new(?MESSAGE_HEAD_ETS, [named_table, public, set, {keypos, 2}]),
+        ets:new(?MESSAGE_DEFINE_ETS, [named_table, public, set, {keypos, 2}]),
+        ets:new(?MESSAGE_TYPE_ETS, [named_table, public, set, {keypos, 2}])
+    catch
+        E:R -> io:format("create ets error :~p~n", [{E, R}])
+    end,
+    todo.
