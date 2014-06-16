3d769cbd05ba58b949b489e189eb4fb560aa7db2
EricYT <eric.yutao@gmail.com>
2014-06-16 23:28:51 +0800

    add sql generated model

---
+++ src/mysql_code_gen/mysql_sql_gen.erl
@@ -0,0 +1,16 @@
+%% @author Eric.yutao
+%% @doc @todo Add description to mysql_sql_gen.
+
+
+-module(mysql_sql_gen).
+-author('eric.yutao').
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([]).
+
+-compile(export_all).
+
+gen_sql_file() ->
+    todo.
