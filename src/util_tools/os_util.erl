95a112e3fcf9ba7a28b6ada580bfc7da66ae6c8d
余涛 <eric.yutao@gmail.com>
2014-12-21 00:17:32 +0800

    nothing

--- src/util_tools/os_util.erl
+++ src/util_tools/os_util.erl
@@ -76,29 +76,28 @@ win_run(CmdLine, Flag) ->
 
 run_erl(Hiden, Name, Host, MnesiaDir, SmpEnable, Wait, Option) ->
 	CommandLine = get_erl_cmd(Hiden, Name, Host, MnesiaDir, SmpEnable, Wait, Option),
-	io:format("Command ~p~n", [CommandLine]).
-%% 	case Wait of
-%% 		wait ->
-%% 			wait_exe(CommandLine);
-%% 		nowait ->
-%% 			run_exe(CommandLine)
-%% 	end.
+	io:format("Command ~p~n", [CommandLine]),
+	case Wait of
+		wait ->
+			wait_exe(CommandLine);
+		nowait ->
+			run_exe(CommandLine)
+	end.
 
 run_erl(Hiden, Name, Host, MnesiaDir, SmpEnable, Wait, Option, IsShell) ->
 	CommandLine = get_erl_cmd(Hiden, Name, Host, MnesiaDir, SmpEnable, Wait, Option),
 	io:format("Command ~p~n", [CommandLine]),
-	ok.
-%% 	if
-%% 		IsShell ->
-%% 			erl_command:main(CommandLine);
-%% 		true ->
-%% 			case Wait of
-%% 				wait ->
-%% 					wait_exe(CommandLine);
-%% 				nowait ->
-%% 					run_exe(CommandLine)
-%% 			end
-%% 	end.
+	if
+		IsShell ->
+			erl_command:main(CommandLine);
+		true ->
+			case Wait of
+				wait ->
+					wait_exe(CommandLine);
+				nowait ->
+					run_exe(CommandLine)
+			end
+	end.
 
 
 wait_exe(CmdLine) ->
